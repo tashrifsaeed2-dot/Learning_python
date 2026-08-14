8d6d1c80689006dff026b7e9dfc515d12c621149
tashrifsaeed2-dot <tashrifsaeed2@gmail.com>
2026-08-14 23:06:26 +0600

    Add files via upload

---
+++ Intro_to_python/test.py
@@ -0,0 +1,11 @@
+# Printing a greeting message
+print("Hello World")
+
+# Printing amy name 
+print("My name is Tashrif")
+
+# Printing a fun fact about myself
+print("I love playing football")
+
+# Performimg and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
