39d8c30abb7d7fcdb45c8934828d3d8d245fc4a6
tashrifsaeed2-dot <tashrifsaeed2@gmail.com>
2026-08-15 23:07:48 +0600

    Add files via upload

---
+++ Python_Learning/Getting started with programming/variable.py
@@ -0,0 +1,21 @@
+""" 
+Variable Definition: A variable is a named location/storage/conatiner inside computer's memory where we can one or more data/values/information and we can update those when we need it.
+"""
+
+#Valid Variables
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# #Invalid variable names
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+#There are some reserved words in Phyton(as if those are pytthos's personal property)
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
